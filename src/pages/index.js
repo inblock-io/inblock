f386c77da6884f602ea105477225357588d3c0c4
inblock-io <56962146+inblock-io@users.noreply.github.com>
2020-04-01 00:41:11 +0200

    The error was on contentful.
    For an unknown reason the build would not complete if the content models Mission and Events contained no items. Even though the lines corresponding to them in the index.js were commented. Perhaps they were referenced in a later section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ class RootIndex extends React.Component {
           <Introduction introductionInfo={firstView.node} />
         </Container>
 
-        <MissionSection mission={missionInfo.node}/>
+        {/* <MissionSection mission={missionInfo.node}/> */}
 
         <a className="anchor" id="services"></a>
         <Services services={services}/>
@@ -49,7 +49,7 @@ class RootIndex extends React.Component {
           videos={videos}
           />
 
-        <Events events={events}/>
+        {/* <Events events={events}/> */}
 
         <Partners partners={partners}/>
 
