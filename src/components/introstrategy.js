93c50aa768ca2084f96b59b846e6914df4e7690b
le-dawg <dawid.golebiewski@tuhh.de>
2020-06-30 19:44:34 +0200

    Contentfl content not showing. image alignment in bootstrap rows in services.js questionable

--- src/components/introstrategy.js
+++ src/components/introstrategy.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import styles from './introstrategy.module.css'
+import Container from '../components/container'
 
 export default ({ introstrategyInfo }) => (
-  <div className={`row ${styles.introstrategy}`}>
-    <div className={styles.background}></div>
-    <div className="col-lg-6">
-      <p>{introstrategyInfo.subtitle1}</p>
-      <h1 className={styles.title}>{introstrategyInfo.mainTitle}</h1>
-      <p className={styles.subtitle}>{introstrategyInfo.subtitle2}</p>
-    </div>
-    <div className="col-lg-6">
-      <img className={styles.introImg} src={introstrategyInfo.introductionImg.sizes.src} alt=""/>
+  <Container>
+  <div className={`row ${styles.fullintro_decoration_row}`}>
+    <div className="col-8">
+      <h3 className="">Not showing{introstrategyInfo.subtitle}</h3>
+      <p>Also not showing{introstrategyInfo.title}</p>
     </div>
   </div>
-
+  </Container>
 )
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,26 +1,45 @@
 import React from 'react'
 import get from 'lodash/get'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Introduction from '../components/introduction'
 import Services from '../components/services'
 import Container from '../components/container'
 import MissionSection from '../components/mission'
 import styles from '../templates/service.module.css'
+import StrategyIntro from '../components/introstrategy'
 
 class ServicesPage extends React.Component {
   render() {
     /* Gets data via graphQL queries defined at the bottom of the page */
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const services = get(this, 'props.data.allContentfulOurServices.edges')
+    const [servicesOverview] = get(this, 'props.data.allContentfulServicesOverview.edges')
+    const [missionInfo] = get(this, 'props.data.allContentfulMission.edges')
 
     return (
       <div className="page">
         <Helmet title={siteTitle} />
         <div className="title-bg text-center">
-          <h1 className={`title ${styles.title}`}>Services</h1>
+          <h1 className={`title ${styles.title}`}>Our Services</h1>
         </div>
 
+        {/*<MissionSection mission={missionInfo.node}/>*/}
+        
+        <StrategyIntro introstrategyInfo={servicesOverview.node}/>
+        
+        {/*<Container>
+          <div className="row">
+            <div className="col-12">
+              <div className="">
+                <h3 className="">S</h3>
+              </div>
+            </div>
+          </div>
+        </Container>*/}
+
         <Services services={services}/>
+        
       </div>
     )
   }
@@ -44,6 +63,28 @@ export const pageQuery = graphql`
         }
       }
     }
+    allContentfulMission(limit: 1) {
+      edges {
+        node {
+          subtitle
+          title
+          descriptionHalf {
+            content{
+              content{
+                value
+              }
+            }
+          }
+          descriptionHalf2 {
+            content{
+              content{
+                value
+              }
+            }
+          }
+        }
+      }
+    }
     allContentfulOurServices(limit:2, sort: { fields: [createdAt], order: ASC}){
       edges{
         node{
@@ -56,6 +97,19 @@ export const pageQuery = graphql`
               }
             }
           }
+        }
+      }
+    }
+    allContentfulServicesOverview {
+      edges {
+        node {
+          title
+          subtitle
+          childContentfulServicesOverviewIntrotextTextNode {
+            childMarkdownRemark {
+              html
+          }
+        }
         }
       }
     }
